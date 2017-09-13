b9b0a070ef46b62395cda60ffce265c754ea67f7
sunnydgoswami <sunnydgoswami@gmail.com>
2017-09-13 14:54:01 -0400

    Updated views and functionality...use this only replace original master this is better

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,16 +1,19 @@
 (function() {
-    function HomeCtrl(Task, $uibModal) {
+    function HomeCtrl(Task) {
       this.tasks = Task.all;
       this.currentTask = null;
       // this.currentUser = $cookies.get('blocitOffCurrentUser') make this later using uicookies.
 
-      this.openNewTask = function () {
-          $uibModal.open({
-              templateUrl: 'templates/modal.html',
-              size: 'lg',
-              controller: 'ModalCtrl as modal'
-          });
+      this.totalTasks = function() {
+        return this.tasks.length;
       }
+
+      this.createTask = function() {
+        Task.add(this.newTask);
+        this.newTask = {name: "" };
+      }
+
+
 
       this.storeTaskName = function (task) {
         this.currentTask = task;
@@ -25,5 +28,58 @@
 
     angular
         .module('blocitOff')
-        .controller('HomeCtrl', ['Task', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Task', HomeCtrl]);
 })();
+
+
+
+
+
+// (function() {
+//     function HomeCtrl($scope, $firebaseArray) {
+//         $scope.filterName = {};
+//
+//         var ref = firebase.database().ref().child("Tasks");
+//         var sync = $firebaseArray(ref);
+//
+//         $scope.totalTask = function() {
+//             return $scope.tasks.length;
+//         };
+//
+//         $scope.toggleDone = function(item) {
+//             item.done = !item.done;
+//             $scope.tasks.$save(item);
+//         };
+//
+//         $scope.addTodo = function(task) {
+//             $scope.tasks.$add(task);
+//             $scope.newItem = "";
+//         };
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//       // this.tasks = Task.all;
+//       // this.currentTask = null;
+//       // // this.currentUser = $cookies.get('blocitOffCurrentUser') make this later using uicookies.
+//       //
+//       // this.storeTaskName = function (task) {
+//       //   this.currentTask = task;
+//       // }
+//
+//       this.isCollapsed = true;
+//       this.status = {
+//         isopen: false
+//       }
+//
+//     }
+//
+//     angular
+//         .module('blocitOff')
+//         .controller('HomeCtrl', ['$scope', '$firebaseArray', HomeCtrl]);
+// })();
