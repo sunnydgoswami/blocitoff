bee0cde2d37e256ed8d8898cda4723cdda44d8ec
sunnydgoswami <sunnydgoswami@gmail.com>
2017-09-15 18:30:41 -0400

    Items are being removed from the list once they are checked off as completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -13,6 +13,13 @@
         this.tasks.$save(task);
       };
 
+//come back to later it is not access done value
+      // this.totalCompleted = function() {
+      //   return _.filter(this.tasks, function(task) {
+      //     return this.task.done == true;
+      //   }).length;;
+      // };
+
       this.createTask = function() {
         Task.add({
           name: this.newTask,
@@ -42,56 +49,3 @@
         .module('blocitOff')
         .controller('HomeCtrl', ['Task', HomeCtrl]);
 })();
-
-
-
-
-
-// (function() {
-//     function HomeCtrl($scope, $firebaseArray) {
-//         $scope.filterName = {};
-//
-//         var ref = firebase.database().ref().child("Tasks");
-//         var sync = $firebaseArray(ref);
-//
-//         $scope.totalTask = function() {
-//             return $scope.tasks.length;
-//         };
-//
-//         $scope.toggleDone = function(item) {
-//             item.done = !item.done;
-//             $scope.tasks.$save(item);
-//         };
-//
-//         $scope.addTodo = function(task) {
-//             $scope.tasks.$add(task);
-//             $scope.newItem = "";
-//         };
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//       // this.tasks = Task.all;
-//       // this.currentTask = null;
-//       // // this.currentUser = $cookies.get('blocitOffCurrentUser') make this later using uicookies.
-//       //
-//       // this.storeTaskName = function (task) {
-//       //   this.currentTask = task;
-//       // }
-//
-//       this.isCollapsed = true;
-//       this.status = {
-//         isopen: false
-//       }
-//
-//     }
-//
-//     angular
-//         .module('blocitOff')
-//         .controller('HomeCtrl', ['$scope', '$firebaseArray', HomeCtrl]);
-// })();
